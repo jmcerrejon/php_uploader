a2430baf5e9c4e7be7436432a9de04bf388d2f09
Jose Manuel Cerrejon Gonzalez <ulysess@gmail.com>
2018-03-08 10:13:36 +0100

    Implemented storage & added .env.example

--- upload-manager.php
+++ upload-manager.php
@@ -7,20 +7,19 @@ $dotenv->load();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_FILES["doc"]) && $_FILES["doc"]["error"] == 0) {
-        $filename = $_FILES["doc"]["name"];
-        $filetype = $_FILES["doc"]["type"];
-        $filesize = $_FILES["doc"]["size"];
+        $fileName = $_FILES["doc"]["name"];
+        $fileSize = $_FILES["doc"]["size"];
+        $folderPath = $_ENV['FOLDER_PATH'];
 
         $maxsize = $_ENV['MAX_FILE_SIZE'] * 1024 * 1024;
-        if ($filesize > $maxsize) {
+        if ($fileSize > $maxsize) {
             die("Error: File size is larger than the allowed limit.");
         }
 
-        // Check whether file exists before uploading it
-        if (file_exists("storage/" . $_FILES["doc"]["name"])) {
-            echo $_FILES["doc"]["name"] . " is already exists.";
+        if (file_exists($folderPath.$fileName)) {
+            echo "$fileName is already exists.";
         } else {
-            move_uploaded_file($_FILES["doc"]["tmp_name"], "storage/" . $_FILES["doc"]["name"]);
+            move_uploaded_file($_FILES["doc"]["tmp_name"], $folderPath . $fileName);
             echo "Your file was uploaded successfully. <br><a href='#' onclick='window.history.back();'>Back</a>";
         }
     } else {
